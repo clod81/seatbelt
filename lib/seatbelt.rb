66c372badc41bddbf78e9dc3a3f17a053caf14a3
Claudio Contin <contin@gmail.com>
2014-04-24 09:23:14 +1200

    Update seatbelt.rb
    works properly under minitest 5

--- lib/seatbelt.rb
+++ lib/seatbelt.rb
@@ -12,17 +12,9 @@ if defined?(Test)
 end
 
 if defined?(Minitest)
-  if defined?(Minitest::Unit)
-    class Minitest::Unit::TestCase
-      include Seatbelt::AssertContentType if defined?(ActionController)
-      include Seatbelt::AssertJson
-      include Seatbelt::AssertMail if defined?(ActionMailer)
-    end
-  else # Minitest >= 5.x
-    class Minitest::Test
-      include Seatbelt::AssertContentType if defined?(ActionController)
-      include Seatbelt::AssertJson
-      include Seatbelt::AssertMail if defined?(ActionMailer)
-    end
+  class Minitest::Test
+    include Seatbelt::AssertContentType if defined?(ActionController)
+    include Seatbelt::AssertJson
+    include Seatbelt::AssertMail if defined?(ActionMailer)
   end
 end
